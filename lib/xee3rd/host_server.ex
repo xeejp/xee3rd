7232c9122b01bd61b17eafbe63423cffb11cd2d3
ryo33 <ryo12redstone@gmail.com>
2015-12-23 13:25:55 +0900

    Modify HostServer

--- lib/xee3rd/host_server.ex
+++ lib/xee3rd/host_server.ex
@@ -3,23 +3,34 @@ defmodule Xee3rd.HostServer do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
-  def register(host_id, experiment_info) do
+  def register(host_id, experiment_id) do
     Agent.update(__MODULE__, fn map ->
       (
       list = if Map.has_key?(map, host_id), do: map[host_id], else: []
-      Map.put(map, host_id, List.insert_at(list, 0, experiment_info))
+      Map.put(map, host_id, List.insert_at(list, 0, experiment_id))
       ) end)
   end
 
+  @doc "Get all of experiments created by the host."
   def get(host_id) do
     Agent.get(__MODULE__, fn map ->
       map[host_id]
     end)
   end
 
-  def drop(host_id) do
+  @doc "Check whether x_id exists or not."
+  def has?(host_id, x_id) do
+    Agent.get(__MODULE__, fn map -> Enum.member?(map[host_id], x_id) end)
+  end
+
+  @doc "Remove the experiment_id."
+  def drop(host_id, x_id) do
     Agent.update(__MODULE__, fn map ->
-      Map.delete(map, host_id)
+      if Enum.member?(map, host_id) do
+        Map.put(map, host_id, Enum.filter(map[host_id], fn x -> x != x_id end))
+      else
+        map
+      end
     end)
   end
 end
